6432755f9536c00970ffe0ddb82f1fecc7177262
Celokuhle <Celokuhle.myeza@gmail.com>
2016-08-25 22:45:05 +0500

    added a new field in JobInvitatioTable

---
+++ TempJobsWcf/TempJobsWcf/Invitation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TempJobsWcf
+{
+    public class Invitation
+    {
+        private int JobID;
+        private string Name;
+        private string Description;
+        private int NumberOfDays;
+        private string DueDate;
+        private string StartDate;
+        private string StartTime;
+        private string EndTime;
+        private string Location;
+        private double ToBePaid;
+        private int EmployerID;
+        private string EmployerFirstName;
+        private string EmployerLastName;
+
+
+        
+
+    }
+}
